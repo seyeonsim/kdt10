1ce09aa49857d32ccaa9ee76ab0c11d50774db85
seyeonsim <seyeonsimw@gmail.com>
2023-12-16 12:54:52 +0900

    study : node.js 231121

---
+++ 07-3-express/231121_app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+// express에 사용할 템플릿 엔진 종류를 ejs로 등록
+app.set('view engine', 'ejs');
+// 템플릿 엔진 파일(.ejs)을 저장할 위치
+app.set('views', './views');
+
+// static 미들웨어 등록(정적 파일 로드 ex. css, js)
+// static 이라는 실제 폴더를 static 이라는 이름으로 접근하겠다
+    // ex. app.use('/static', express.static(__dirname + '/new')) 
+    // : new 라는 이름을 가진 실제 폴더를 static 이라는 이름으로 접근하겠다
+app.use('/static', express.static(__dirname + '/static')) 
+// __dirname : 현재 이 js가 포함되어있는 파일 전체 = 07-3-express 폴더
+console.log(__dirname);  // 확인해보기
+
+// app.get(경로, 해당 경로로 들어왔을 때 실행할 콜백 함수)
+// localhost:8000/ 경로로 접속했을 때
+app.get('/', function(req, res) {
+    // send(응답 내용)
+    // res.send('<h1>Hello Express!</h1>');
+
+    //231121_index라는 파일명을 찾아서 해당 파일을 렌더
+    res.render('231121_index')
+})
+
+app.get('/kdt', function(req, res) {
+    // send(응답 내용)
+    res.send('<h1>Hello Express kdt!</h1>');
+})
+
+
+
+// 포트 열기
+app.listen(PORT, function() {
+    console.log(`server listening on ${PORT}`);
+})
